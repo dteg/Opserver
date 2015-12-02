a6ed7e7aa12b7d989559cc42b9a77cf19319c395
Mario Ezra <thetest3r@gmail.com>
2015-12-02 13:53:34 -0500

    Added unit tests

---
+++ opserver.Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opserver.Entity;
+using System.Linq;
+
+namespace opserver.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ContextConnection()
+        {
+            bool isValid = false;
+            try
+            {
+                var context = new Entities();
+                context.Nodes.FirstOrDefault();
+                isValid = true;
+            }
+            catch
+            {
+                isValid = false;
+            }
+
+            Assert.IsTrue(isValid);
+        }
+
+        public void SaveSnapshot()
+        {
+
+        }
+
+        public void PullSnapshot()
+        {
+
+        }
+
+        //Save snapshot, another one, and pull previous snapshot
+        public void SaveSavePull()
+        {
+
+        }
+
+    }
+}
